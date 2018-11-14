051f22a87fbf238b5b36d0caa9df0a10cd2cca14
Nick Teissler <nteissler@bignerdranch.com>
2018-11-14 14:54:07 -0600

    Initial Commit

---
+++ The Shell/ViewController.swift
@@ -0,0 +1,16 @@
+//
+// Created by Nick Teissler on 11/14/18
+// Copyright © 2018 Big Nerd Ranch. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
