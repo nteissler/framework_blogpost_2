abb937ed0aef22c3d9e6c1015c7aed8293ffb082
Nick Teissler <nteissler@bignerdranch.com>
2018-11-26 09:49:37 -0600

    Modularize the framework

---
+++ Frameworks/Coxswain.framework/Headers/Coxswain.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Coxswain.
+FOUNDATION_EXPORT double CoxswainVersionNumber;
+
+//! Project version string for Coxswain.
+FOUNDATION_EXPORT const unsigned char CoxswainVersionString[];
